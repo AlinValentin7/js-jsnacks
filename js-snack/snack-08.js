e49b4bf20df85636325250c21607d7c4797f72c7
AlinValentin7 <trandafiralionvalentin@gmail.com>
2024-07-11 00:11:41 +0200

    ADD:js-snack8

---
+++ js-snack/snack-08.js
@@ -0,0 +1,24 @@
+console.log('snack 8')
+
+/*snack 08
+Fate generare un numero random da 0 a 10 al computer,
+ e chiedete all’utente di inserire un suo numero. 
+ Se il numero scelto dall’utente è uguale al numero del computer informate l’utente che ha vinto,
+  altrimenti ha perso.
+A questo punto fate giocare l’utente,
+ fintanto che non vince la partita contro il PC */
+
+let numeroCasuale = Math.round(Math.random() * 10 )
+ console.log(numeroCasuale)
+for(;;){
+
+ let numeroUtente = parseFloat(prompt('inserisci un numero da 1 a 10'))
+
+ if(numeroUtente === numeroCasuale ){
+    console.log('Hai vinto!')
+    break
+ }
+ else {
+    console.log('Hai perso!')
+ }
+}
